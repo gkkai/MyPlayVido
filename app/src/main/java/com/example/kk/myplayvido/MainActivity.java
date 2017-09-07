5cff045085f97151b67184bac3cadb4f6b7a503d
gkkai <gkkai@yitong.com.cn>
2017-09-07 10:25:56 +0800

    test

--- app/src/main/java/com/example/kk/myplayvido/MainActivity.java
+++ app/src/main/java/com/example/kk/myplayvido/MainActivity.java
@@ -79,7 +79,7 @@ private Button bt;
     private void startGetImageThread() {
         Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
         final VideoListAdapter  adapter = new VideoListAdapter(this, cursor);
-        listView.setAdapter(adapter);
+        //listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
